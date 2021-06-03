a2bdc39eaf0554cbb17fec2413e5ff41a9b4bc3d
Shubhaankar <shubhaankar@hotmail.com>
2021-06-03 18:13:53 +0530

    added basic stuff in template

--- TWT/apps/timathon/views/codjam_listview.py
+++ TWT/apps/timathon/views/codjam_listview.py
@@ -2,6 +2,9 @@
 This has not been completed
 This is for past codejams and showing the leaderboard
 """
+from allauth.socialaccount.models import SocialAccount
+
+from random import randint
 from django.contrib import messages
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
@@ -11,6 +14,14 @@ from ...challenges.models.challenge import Challenge
 from ..models.team import Team
 from ..models.submission import Submission
 from TWT.context import get_discord_context
+
+
+class PastChallenge:
+
+
+    def __init__(self, challenge, submissions):
+        self.challenge = challenge
+        self.submissions = submissions
 
 
 class PreviousView(View):
@@ -29,7 +40,26 @@ class PreviousView(View):
             submissions = Submission.objects.filter(
                 team__in=Team.objects.filter(challenge=challenge, submitted=True).exclude(winner=0)
                 )
-            challenge_list.append({'challenge': challenge, 'submissions': submissions})
+            for submission in submissions:
+                discord_members = []
+                for member in submission.team.members.all():
+                    new_member = {}
+                    try:
+                        user = SocialAccount.objects.get(user_id=member.id)
+                    except SocialAccount.DoesNotExist:
+                        pass
+                    else:
+                        new_member["user_id"] = user.uid
+                        avatar_url = user.get_avatar_url()
+                        if avatar_url.endswith("None.png"):
+                            random = randint(0, 4)
+                            avatar_url = f'https://cdn.discordapp.com/embed/avatars/{random}.png'
+                        new_member["avatar_url"] = avatar_url
+                        new_member["username"] = user.extra_data["username"]
+                        new_member["discriminator"] = user.extra_data["discriminator"]
+                    discord_members.append(new_member)
+                submission.team.discord_members = discord_members
+            challenge_list.append(PastChallenge(challenge=challenge,submissions=submissions))
         context['challenge_list'] = challenge_list
         # print(context)
         return render(request,'timathon/codejam_listView.html',context)
